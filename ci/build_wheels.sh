6b49f8335c2199143f05feb7224f9f7037a2827c
Vincent Tavernier <vince.tavernier@gmail.com>
2020-07-16 00:19:49 +0200

    Strip binaries when building wheels

--- ci/build_wheels.sh
+++ ci/build_wheels.sh
@@ -16,7 +16,7 @@ for PYBIN in /opt/python/cp{35,36,37,38,39}*/bin; do
     export PYTHON_LIB=$(${PYBIN}/python -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
     export LIBRARY_PATH="$LIBRARY_PATH:$PYTHON_LIB"
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PYTHON_LIB"
-    $MATURIN build -i $PYTHON_SYS_EXECUTABLE --cargo-extra-args='--features python'
+    $MATURIN build --strip --cargo-extra-args='--features python' --release -i $PYTHON_SYS_EXECUTABLE
 done
 
 # We're building in Docker but we want outside to access the wheels directory
