af180df197f0e83b82359a9ad1668eced84d3b72
Vincent Tavernier <vince.tavernier@gmail.com>
2021-02-14 13:27:45 +0100

    Remove useless lazy_static

--- glslt/src/transform/instantiate.rs
+++ glslt/src/transform/instantiate.rs
@@ -1,46 +1,43 @@
 use glsl_lang::{ast::*, visitor::*};
 
 use indexmap::IndexMap;
-use lazy_static::lazy_static;
 
 use crate::{glsl_ext::FunIdentifierExt, Error, Result};
 
 use super::{template::TemplateDefinition, Scope};
 
-lazy_static! {
-    // Keep this sorted
-    #[rustfmt::skip]
-    static ref BUILTIN_FUNCTION_NAMES: &'static [&'static str] = &[
-        "EmitStreamVertex", "EmitVertex", "EndPrimitive", "EndStreamPrimitive", "abs", "acos",
-        "acosh", "all", "any", "asin", "asinh", "atan", "atanh", "atomicAdd", "atomicAnd",
-        "atomicCompSwap", "atomicCounter", "atomicCounterDecrement", "atomicCounterIncrement",
-        "atomicExchange", "atomicMax", "atomicMin", "atomicOr", "atomicXor", "barrier", "bitCount",
-        "bitfieldExtract", "bitfieldInsert", "bitfieldReverse", "ceil", "clamp", "cos", "cosh",
-        "cross", "dFdx", "dFdxCoarse", "dFdxFine", "dFdy", "dFdyCoarse", "dFdyFine", "degrees",
-        "determinant", "distance", "dot", "equal", "exp", "exp2", "faceforward", "findLSB",
-        "findMSB", "floatBitsToInt", "floatBitsToUint", "floor", "fma", "fract", "frexp", "fwidth",
-        "fwidthCoarse", "fwidthFine", "greaterThan", "greaterThanEqual", "groupMemoryBarrier",
-        "imageAtomicAdd", "imageAtomicAnd", "imageAtomicCompSwap", "imageAtomicExchange",
-        "imageAtomicMax", "imageAtomicMin", "imageAtomicOr", "imageAtomicXor", "imageLoad",
-        "imageSamples", "imageSize", "imageStore", "imulExtended", "intBitsToFloat",
-        "interpolateAtCentroid", "interpolateAtOffset", "interpolateAtSample", "inverse",
-        "inversesqrt", "isinf", "isnan", "ldexp", "length", "lessThan", "lessThanEqual", "log",
-        "log2", "matrixCompMult", "max", "memoryBarrier", "memoryBarrierAtomicCounter",
-        "memoryBarrierBuffer", "memoryBarrierImage", "memoryBarrierShared", "min", "mix", "mod",
-        "modf", "noise", "noise1", "noise2", "noise3", "noise4", "normalize", "not", "notEqual",
-        "outerProduct", "packDouble2x32", "packHalf2x16", "packSnorm2x16", "packSnorm4x8",
-        "packUnorm", "packUnorm2x16", "packUnorm4x8", "pow", "radians", "reflect", "refract",
-        "removedTypes", "round", "roundEven", "sign", "sin", "sinh", "smoothstep", "sqrt", "step",
-        "tan", "tanh", "texelFetch", "texelFetchOffset", "texture", "textureGather",
-        "textureGatherOffset", "textureGatherOffsets", "textureGrad", "textureGradOffset",
-        "textureLod", "textureLodOffset", "textureOffset", "textureProj", "textureProjGrad",
-        "textureProjGradOffset", "textureProjLod", "textureProjLodOffset", "textureProjOffset",
-        "textureQueryLevels", "textureQueryLod", "textureSamples", "textureSize", "transpose",
-        "trunc", "uaddCarry", "uintBitsToFloat", "umulExtended", "unpackDouble2x32",
-        "unpackHalf2x16", "unpackSnorm2x16", "unpackSnorm4x8", "unpackUnorm", "unpackUnorm2x16",
-        "unpackUnorm4x8", "usubBorrow",
-    ];
-}
+// Keep this sorted
+#[rustfmt::skip]
+static BUILTIN_FUNCTION_NAMES: &'static [&'static str] = &[
+    "EmitStreamVertex", "EmitVertex", "EndPrimitive", "EndStreamPrimitive", "abs", "acos",
+    "acosh", "all", "any", "asin", "asinh", "atan", "atanh", "atomicAdd", "atomicAnd",
+    "atomicCompSwap", "atomicCounter", "atomicCounterDecrement", "atomicCounterIncrement",
+    "atomicExchange", "atomicMax", "atomicMin", "atomicOr", "atomicXor", "barrier", "bitCount",
+    "bitfieldExtract", "bitfieldInsert", "bitfieldReverse", "ceil", "clamp", "cos", "cosh",
+    "cross", "dFdx", "dFdxCoarse", "dFdxFine", "dFdy", "dFdyCoarse", "dFdyFine", "degrees",
+    "determinant", "distance", "dot", "equal", "exp", "exp2", "faceforward", "findLSB",
+    "findMSB", "floatBitsToInt", "floatBitsToUint", "floor", "fma", "fract", "frexp", "fwidth",
+    "fwidthCoarse", "fwidthFine", "greaterThan", "greaterThanEqual", "groupMemoryBarrier",
+    "imageAtomicAdd", "imageAtomicAnd", "imageAtomicCompSwap", "imageAtomicExchange",
+    "imageAtomicMax", "imageAtomicMin", "imageAtomicOr", "imageAtomicXor", "imageLoad",
+    "imageSamples", "imageSize", "imageStore", "imulExtended", "intBitsToFloat",
+    "interpolateAtCentroid", "interpolateAtOffset", "interpolateAtSample", "inverse",
+    "inversesqrt", "isinf", "isnan", "ldexp", "length", "lessThan", "lessThanEqual", "log",
+    "log2", "matrixCompMult", "max", "memoryBarrier", "memoryBarrierAtomicCounter",
+    "memoryBarrierBuffer", "memoryBarrierImage", "memoryBarrierShared", "min", "mix", "mod",
+    "modf", "noise", "noise1", "noise2", "noise3", "noise4", "normalize", "not", "notEqual",
+    "outerProduct", "packDouble2x32", "packHalf2x16", "packSnorm2x16", "packSnorm4x8",
+    "packUnorm", "packUnorm2x16", "packUnorm4x8", "pow", "radians", "reflect", "refract",
+    "removedTypes", "round", "roundEven", "sign", "sin", "sinh", "smoothstep", "sqrt", "step",
+    "tan", "tanh", "texelFetch", "texelFetchOffset", "texture", "textureGather",
+    "textureGatherOffset", "textureGatherOffsets", "textureGrad", "textureGradOffset",
+    "textureLod", "textureLodOffset", "textureOffset", "textureProj", "textureProjGrad",
+    "textureProjGradOffset", "textureProjLod", "textureProjLodOffset", "textureProjOffset",
+    "textureQueryLevels", "textureQueryLod", "textureSamples", "textureSize", "transpose",
+    "trunc", "uaddCarry", "uintBitsToFloat", "umulExtended", "unpackDouble2x32",
+    "unpackHalf2x16", "unpackSnorm2x16", "unpackSnorm4x8", "unpackUnorm", "unpackUnorm2x16",
+    "unpackUnorm4x8", "usubBorrow",
+];
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct CapturedParameter {
